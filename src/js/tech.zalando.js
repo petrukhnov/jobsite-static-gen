6abf23397f55d20a7a391bafa7c1cbb1c8394a9b
Magnus Nilsson <magnus.nilsson@futurice.com>
2015-04-21 18:10:03 +0200

    only show cookie warning for new visitors

--- src/js/tech.zalando.js
+++ src/js/tech.zalando.js
@@ -1,15 +1,39 @@
-var test = 'TEST3';
-
-(function closeCookieBarOnClick($) {
-    var closeBar = function () {
+(function ($) {
+    var cookieName = "zalandoCookieWarning";
+    
+    var hideCookieBar = function () {
         $(".cookie-bar").hide();
     };
 
-    var setCookie = function (days) {
+    var getCookie = function (cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i=0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1);
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    };
+    
+    // Show the cookie warning if there is no cookie
+    (function () {
+        var oldCookie = getCookie(cookieName);
+        if (oldCookie === "") {
+            $(".cookie-bar").show();
+        }
+    })();
+
+    // Set the cookie
+    (function (days) {
         var d = new Date();
         d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
         var expires = "expires=" + d.toGMTString();
-        document.cookie = "cookiewarning=1;" + expires;
-    };
-    $(".cookie-bar").click(closeBar);
+        document.cookie = cookieName + "=1;" + expires;
+    })(365);
+    
+    // add handler to hide the warning
+    $(".cookie-bar").click(hideCookieBar);
 }(jQuery));
