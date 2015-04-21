53deb06c9a3dd7b7eba58af9dadd24813490fbdb
Magnus Nilsson <magnus.nilsson@futurice.com>
2015-04-21 09:53:07 +0200

    start for cookie warning

--- src/js/tech.zalando.js
+++ src/js/tech.zalando.js
@@ -1,1 +1,15 @@
 var test = 'TEST3';
+
+(function closeCookieBarOnClick($) {
+    var closeBar = function () {
+        $(".cookie-bar").hide();
+    };
+
+    var setCookie = function (days) {
+        var d = new Date();
+        d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = "cookiewarning=1;" + expires;
+    };
+    $(".cookie-bar").click(closeBar);
+}(jQuery));
