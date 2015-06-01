0dc2997db954a72e6d27ba9d2807de39ce6eec29
Michael Tauer <michael.tauer@futurice.com>
2015-06-01 14:25:27 +0200

    Use link tag in blog post cards in 'Blog' page

--- src/js/blogpostCard.jsx
+++ src/js/blogpostCard.jsx
@@ -4,15 +4,12 @@ var techZalando = techZalando || {};
     'use strict';
 
     techZalando.BlogpostCard = React.createClass({
-        onCardClick: function() {
-            location.href = '../blog/' + this.props.viewModel.slug;
-        },
-
         render: function () {
             var model = this.props.viewModel,
                 mediaStyle = { backgroundImage: 'url(' + model.thumbnail + ')' },
                 categoryLink = '',
-                subTitle = 'by ' + model.authorNames.join(', ') + ' - ' + model.date;
+                subTitle = 'by ' + model.authorNames.join(', ') + ' - ' + model.date,
+                link = '../blog/' + model.slug;
 
             if (model.category && model.category_link) {
                 categoryLink = <a href={model.category_link} className="category">{model.category}</a>;
@@ -21,18 +18,20 @@ var techZalando = techZalando || {};
             return (
                 <div className="card-container">
                     {categoryLink}
-                    <div className="card blog" onClick={this.onCardClick}>
-                        <div className="media" style={mediaStyle}></div>
-                        <div className="content">
-                            <div className="content-header">
-                                <p className="title">{model.title}</p>
-                                <p className="subtitle">{subTitle}</p>
-                            </div>
-                            <div className="text bodycopy">
-                                <p>{model.description}</p>
+                    <a href={link}>
+                        <div className="card blog">
+                            <div className="media" style={mediaStyle}></div>
+                            <div className="content">
+                                <div className="content-header">
+                                    <p className="title">{model.title}</p>
+                                    <p className="subtitle">{subTitle}</p>
+                                </div>
+                                <div className="text bodycopy">
+                                    <p>{model.description}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             );
         }
