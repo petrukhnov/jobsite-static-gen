1ba98d205e312ab464157b679a8f109f4b9a9e7d
Michael Tauer <michael.tauer@futurice.com>
2015-06-09 09:33:08 +0200

    Render blog posts

--- src/js/blogPage.jsx
+++ src/js/blogPage.jsx
@@ -4,7 +4,7 @@ var techZalando = techZalando || {};
 (function () {
     'use strict';
 
-    var BLOG_POSTS_DATA_RELATIVE_URL = 'js/data/jobsData.json';
+    var BLOG_POSTS_DATA_RELATIVE_URL = 'js/data/blogPostsData.json';
 
     var ItemsContainer = techZalando.ItemsContainer,
         BlogpostCard = techZalando.BlogpostCard;
@@ -13,17 +13,29 @@ var techZalando = techZalando || {};
             this.options = options || {};
 
             this.init();
-            this.render();
         };
 
     BlogPage.prototype.init = function() {
-        console.log('BlogPage.init', this.options);
-        // TODO
+        this.model = {
+            blogpostViewModelsSignal: Rx.DOM.getJSON(
+                this.options.relative_path_to_root + BLOG_POSTS_DATA_RELATIVE_URL)
+        }
+
+        this.model.blogpostViewModelsSignal
+            .subscribe(function(__) {
+                this.render();
+            }.bind(this))
     };
 
     BlogPage.prototype.render = function() {
-        console.log('BlogPage.render');
-        // TODO
+        // render blog posts container
+        React.render(
+            <ItemsContainer
+                viewModelsSignal={this.model.blogpostViewModelsSignal}
+                itemComponent={BlogpostCard}
+                relativePathToRoot={this.options.relative_path_to_root} />,
+            document.getElementById('blog-posts-content')
+        );
     };
 
 })();
