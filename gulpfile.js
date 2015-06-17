f70e54c2a24a548dc21a25c736519ad298cdd04a
Eemeli Kantola <eemeli.kantola@futurice.com>
2015-06-17 16:09:42 +0200

    Work around a linkResolver missing data problem
    For some reason, and for one particular blog post, we don't
    get the data property in linkResolver. The blog post generation
    itself does work, as does the old blogpost URL, so this
    workaround should be ok for now.

--- gulpfile.js
+++ gulpfile.js
@@ -402,7 +402,7 @@ function generateOldBlogpostUrl(doc, resolvedLink) {
 function getOldSlug(doc) {
     switch (doc.type) {
         case 'blog':
-            return doc.data.oldname && doc.data.oldname.json.asText();
+            return doc.data && doc.data.oldname && doc.data.oldname.json.asText();
         case 'blog-rst':
         case 'blog-md':
             return doc.slug;
